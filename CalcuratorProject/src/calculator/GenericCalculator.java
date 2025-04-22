aa23ba034273678f9eff09cfe22e8e4b8cdb5cbd
천세경 <jay45652@gmail.com>
2025-04-23 05:19:34 +0900

    [feature] 제네릭 메소드를 활용하여 다양한 타입의 피연산자를 받는 계산기 구현

---
+++ CalcuratorProject/src/calculator/GenericCalculator.java
@@ -0,0 +1,59 @@
+package calculator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenericCalculator {
+    private final List<Double> results;
+
+    public <S extends Number, U extends Number> double calculate(S num1, U num2, char operator) {
+        double doubleNum1 = num1.doubleValue();
+        double doubleNum2 = num2.doubleValue();
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = doubleNum1 + doubleNum2;
+                break;
+            case '-':
+                result = doubleNum1 - doubleNum2;
+                break;
+            case '*':
+                result = doubleNum1 * doubleNum2;
+                break;
+            case '/':
+                if (doubleNum2 == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
+
+                result = doubleNum1 / doubleNum2;
+                break;
+            default:
+                System.out.println("[Error] : 알 수 없는 연산자입니다.");
+        }
+
+        this.results.add(result);
+        return result;
+    }
+
+    public GenericCalculator() {
+        this.results = new LinkedList<>();
+    }
+
+    public List<Double> getResults() {
+        return this.results;
+    }
+
+    public void setResults(List<Double> newResults) {
+        this.results.clear();
+        if (newResults != null) {
+            this.results.addAll(newResults);
+        }
+    }
+
+    public void deleteFirstResult() {
+        this.results.remove(0);
+    }
+
+
+}
