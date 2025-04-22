807cfc5d39be33c87370f4c028c14df0f72aaf5c
천세경 <jay45652@gmail.com>
2025-04-22 21:10:55 +0900

    [feature] OperatorType enum에 상수 및 calculaor()메소드 정의

---
+++ CalcuratorProject/src/calculator/OperatorType.java
@@ -0,0 +1,52 @@
+package calculator;
+
+public enum OperatorType {
+    ADD('+') {
+        @Override
+        public double calculate(int a, int b) {
+            return a + b;
+        }
+    },
+    SUBTRACT('-') {
+        @Override
+        public double calculate(int a, int b) {
+            return a - b;
+        }
+    },
+    MULTIPLY('*') {
+        @Override
+        public double calculate(int a, int b) {
+            return a * b;
+        }
+    },
+    DIVIDE('/') {
+        @Override
+        public double calculate(int a, int b) {
+            if (b == 0) {
+                throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            }
+            return (double) a / b;
+        }
+    };
+
+    private final char symbol;
+
+    OperatorType(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static OperatorType fromSymbol(char symbol) {
+        for (OperatorType op : values()) {
+            if (op.symbol == symbol) {
+                return op;
+            }
+        }
+        throw new IllegalArgumentException("알 수 없는 연산자입니다: " + symbol);
+    }
+
+    public abstract double calculate(int a, int b);
+}
